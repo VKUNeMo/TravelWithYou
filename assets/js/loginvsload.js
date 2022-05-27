95487c64d524daa739c0f8cfedb2299e48efa53d
phamnhantucode <bao.trancong.2003@gmail.com>
2022-05-27 16:03:09 +0700

    ...

--- assets/js/loginvsload.js
+++ assets/js/loginvsload.js
@@ -1,5 +1,11 @@
-
+document.body.addEventListener("click", function (e) {
+    if (e.target.matches("#loginform")) {
+        document.getElementById("loginform").style.visibility = "hidden";
+        document.getElementById("signup").style.display = "none";
+    }
+});
 document.getElementById("loginform").style.visibility = "hidden";
+document.getElementById("signup").style.display = "none";
 window.addEventListener("load", setTimeout(function(){  
     document.getElementById("loadpage").style.display="none";
 }, 2000));
@@ -12,6 +18,19 @@ function baodeptrai() {
     }   
     else  {
         document.getElementById("loginform").style.visibility = "hidden";
+        location.reload();
+    }
+}
+
+function dungdeptrai() {
+    let a = document.getElementById("signup").style.display;
+    if (a == "none") {
+        document.getElementById("signup").style.display = "";   
+        // location.reload();
+    }   
+    else  {
+        document.getElementById("signup").style.display = "none";
         // location.reload();
     }
 }
+
