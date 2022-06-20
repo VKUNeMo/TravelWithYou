9fdcf05e77f3d5d441c8c1c5fa71bf9a89048594
phamnhantucode <85236147+phamnhantucode@users.noreply.github.com>
2022-06-20 14:15:59 +0700

    Update loginvsload.js

--- assets/js/loginvsload.js
+++ assets/js/loginvsload.js
@@ -47,24 +47,3 @@ function myFunction() {
     element.classList.toggle("transform_180");
  }
 
-
-// function mouseover() {
-//     if (screen.width > 1080) {
-//         document.getElementById("sear").style.transform = "translateX(-150px)";
-//     document.getElementById("search-id").style.opacity = "1";
-//     }
-// }
-
-// function mouseout() {
-//     if (screen.width > 1080) {
-//     document.getElementById("search-id").style.opacity = "0";
-//     document.getElementById("sear").style.transform = "translateX(0px)";
-//     }
-// }
-
-// var icon__menu = $('#sear')
-// var menu__navbar = $("#navbar__1000");
-    // document.getElementById("sear").click(() => {
-    //     document.getElementById("sear").toggleClass('transform__180');
-    //   menu__navbar.toggleClass('transform__0')
-//    })
